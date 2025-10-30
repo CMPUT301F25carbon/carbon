b26dd1531d34f490c129bcee5e978224f2f48f12
Aahil <aahillegend46@gmail.com>
2025-10-30 15:09:58 -0600

    Add model layer for lottery flow (Event, Entry, EntryStatus, Notification)

---
+++ app/src/main/java/com/example/carbon/model/Entry.java
@@ -0,0 +1,10 @@
+package com.example.carbon.model;
+
+public class Entry {
+    public String id;
+    public String eventId;
+    public String userId;
+    public EntryStatus status;
+
+    public Entry() {}
+}
---
+++ app/src/main/java/com/example/carbon/model/Event.java
@@ -0,0 +1,10 @@
+package com.example.carbon.model;
+
+public class Event {
+    public String id;
+    public String title;
+    public int capacity;
+    public String ownerId;
+
+    public Event() {}
+}
