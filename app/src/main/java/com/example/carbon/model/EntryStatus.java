11a14e81169ccbd2366cb02d5c7d7e81965792d8
Aahil <aahillegend46@gmail.com>
2025-10-30 15:09:58 -0600

    Add model layer for lottery flow (Event, Entry, EntryStatus, Notification)

---
+++ app/src/main/java/com/example/carbon/model/EntryStatus.java
@@ -0,0 +1,3 @@
+package com.example.carbon.model;
+
+public enum EntryStatus { PENDING, WON, LOST }
---
+++ app/src/main/java/com/example/carbon/model/Notification.java
@@ -0,0 +1,12 @@
+package com.example.carbon.model;
+
+public class Notification {
+    public String id;
+    public String userId;
+    public String title;
+    public String body;
+    public long   createdAt;
+    public boolean read;
+
+    public Notification() {}
+}
