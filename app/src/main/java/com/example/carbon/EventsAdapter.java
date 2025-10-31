c9d355c135c6a6708b8c043befa7eb8f868bc2a9
jpark <j.parkstorm@gmail.com>
2025-10-31 00:07:24 -0600

    03.01.01

--- app/src/main/java/com/example/carbon/EventsAdapter.java
+++ app/src/main/java/com/example/carbon/EventsAdapter.java
@@ -3,6 +3,7 @@ package com.example.carbon;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,6 +16,32 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.VH> {
 
     private List<Event> events = new ArrayList<>();
     private final SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd, yyyy", Locale.US);
+    private boolean isEditMode = false;
+    private OnDeleteClickListener deleteListener;
+    private OnEditModeListener editModeListener;
+
+    public List<Event> getEvents() {
+        return events;
+    }
+
+    public interface OnDeleteClickListener {
+        void onDelete(Event event, int position);
+    }
+    public interface OnEditModeListener {
+        void onEditMode();
+    }
+
+    public void setDeleteListener(OnDeleteClickListener listener) {
+        this.deleteListener = listener;
+    }
+    public void setEditModeListener(OnEditModeListener listener) {
+        this.editModeListener = listener;
+    }
+
+    public void setEditMode(boolean editMode) {
+        isEditMode = editMode;
+        notifyDataSetChanged();
+    }
 
     public void updateList(List<Event> newList) {
         events.clear();
@@ -29,8 +56,6 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.VH> {
         return new VH(v);
     }
 
-
-
     @Override
     public void onBindViewHolder(@NonNull VH h, int i) {
         Event e = events.get(i);
@@ -38,18 +63,39 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.VH> {
         h.tvDate.setText(dateFormat.format(e.getEventDate()));
         h.tvLocation.setText(e.getEventLocation() + ", " + e.getEventCity());
         h.tvSpots.setText(e.getTotalSpots() + " spots");
+
+        h.btnDelete.setVisibility(isEditMode ? View.VISIBLE : View.GONE);
+
+        if (isEditMode) {
+            h.btnDelete.setOnClickListener(v -> {
+                if (deleteListener != null) {
+                    deleteListener.onDelete(e, h.getAdapterPosition());
+                }
+            });
+        } else {
+            h.btnDelete.setOnClickListener(null);
+        }
+
+        h.itemView.setOnLongClickListener(v -> {
+            if (editModeListener != null) {
+                editModeListener.onEditMode();
+            }
+            return true;
+        });
     }
 
     @Override public int getItemCount() { return events.size(); }
 
     static class VH extends RecyclerView.ViewHolder {
         TextView tvTitle, tvDate, tvLocation, tvSpots;
+        ImageButton btnDelete;
         VH(@NonNull View v) {
             super(v);
             tvTitle = v.findViewById(R.id.tv_title);
             tvDate = v.findViewById(R.id.tv_date);
             tvLocation = v.findViewById(R.id.tv_location);
             tvSpots = v.findViewById(R.id.tv_spots);
+            btnDelete = v.findViewById(R.id.btn_delete);
         }
     }
 }
