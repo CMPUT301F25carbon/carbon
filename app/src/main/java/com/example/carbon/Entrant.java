7a06cdb408bdf778ab36b970839e7c1d9e4a97bf
matsdehaas <131658515+matsdehaas@users.noreply.github.com>
2025-11-02 11:06:18 -0700

    Finished sign-up validation and therefore the entirety of US 01.02.01

--- app/src/main/java/com/example/carbon/Entrant.java
+++ app/src/main/java/com/example/carbon/Entrant.java
@@ -1,33 +1,4 @@
 package com.example.carbon;
 
 public class Entrant {
-    String firstName;
-    String lastName;
-    String email;
-    String phoneNo;
-    String address1;
-    String address2;
-    String postcode;
-    String city;
-    String province;
-
-    public Entrant(String firstName,
-                   String lastName,
-                   String email,
-                   String phoneNo,
-                   String address1,
-                   String address2,
-                   String postcode,
-                   String city,
-                   String province){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.phoneNo = phoneNo;
-        this.address1 = address1;
-        this.address2 = address2;
-        this.postcode = postcode;
-        this.city = city;
-        this.province = province;
-    }
 }
