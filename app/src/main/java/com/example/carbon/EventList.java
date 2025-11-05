59529a93d599a1c0627166a7175ed9cc76261fd9
Oyonti Nasir <140875445+hridita11@users.noreply.github.com>
2025-11-05 13:02:53 -0700

    01.01.03: To be able to view a list of joinable events

--- app/src/main/java/com/example/carbon/EventList.java
+++ app/src/main/java/com/example/carbon/EventList.java
@@ -1,6 +1,7 @@
 package com.example.carbon;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class EventList {
     private ArrayList<Event> events;
@@ -8,4 +9,26 @@ public class EventList {
     public EventList() {
         events = new ArrayList<>();
     }
+
+    // Add a single event to the list
+    public void addEvent(Event e) {
+        events.add(e);
+    }
+
+    // Return all events
+    public List<Event> getAllEvents() {
+        return events;
+    }
+
+    // Return only the events that are currently open/joinable : US 01.01.03
+    public List<Event> getJoinableEvents() {
+        List<Event> joinable = new ArrayList<>();
+        for (Event e : events) {
+            if (e.getIsActive()) { // uses your Event.getIsActive() method
+                joinable.add(e);
+            }
+        }
+        return joinable;
+    }
+
 }
